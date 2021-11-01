dec35fd4262065d1422ef42dc822db44c0c893a0
HumbertoPeluso <humbertopeluso@yahoo.com.br>
2021-11-01 16:39:50 -0300

    kubeconfig tests

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,21 @@ output "cluster_endpoint" {
   description = "Endpoint da API do cluster Kubernetes."
   value       = aws_eks_cluster.this.endpoint
 }
+
+output "kubeconfig" {
+  description = "kubectl config file contents for this EKS cluster. Will block on cluster creation until the cluster is really ready."
+  value       = local.kubeconfig
+
+  # So that calling plans wait for the cluster to be available before attempting to use it.
+  # There is no need to duplicate this datasource
+  depends_on = [data.http.wait_for_cluster]
+}
+
+output "kubeconfig_filename" {
+  description = "The filename of the generated kubectl config. Will block on cluster creation until the cluster is really ready."
+  value       = concat(local_file.kubeconfig.*.filename, [""])[0]
+
+  # So that calling plans wait for the cluster to be available before attempting to use it.
+  # There is no need to duplicate this datasource
+  depends_on = [data.http.wait_for_cluster]
+}
